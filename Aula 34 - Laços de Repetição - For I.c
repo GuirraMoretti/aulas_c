1f81cc88d96b6bc9880fe8f5a3656cdd6af31219
Guilherme Moretti <68204019+Guirraa@users.noreply.github.com>
2020-12-02 00:42:39 -0300

    AULA_34_a

---
+++ Aula 34 - Laços de Repetição - For I.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    /*
+?   for (parte 1; parte 2; parte 3)
+
+    {
+?        parte 1 -> é aonde nós declaramos e incializamos a nossa variavel
+
+?        parte 2 -> é a condição
+
+?        parte 3 -> é aonde incrementamos a nossa variavél
+    }
+    */
+   for (int i = 0; i < 10; ++i)
+   {
+       printf("%i\n", i);
+   }
+   
+}
+
+//! https://www.youtube.com/watch?v=xykLtbpz6ZQ&list=PLesCEcYj003SwVdufCQM5FIbrOd0GG1M4&index=34&ab_channel=eXcript
+//! http://excript.com/linguagem-c/estrutura-repeticao-for-c.html
